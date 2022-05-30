e7064f965cfed992aed83fa937c76c5803547ed8
Mohamed Elmokhtar INZALE <mokhtarinzale1999@gmail.com>
2022-05-30 17:02:25 +0100

    first commit

---
+++ src/main/java/ma/emsi/tickets/TicketsIncidentsApplication.java
@@ -0,0 +1,13 @@
+package ma.emsi.tickets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketsIncidentsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketsIncidentsApplication.class, args);
+	}
+
+}
